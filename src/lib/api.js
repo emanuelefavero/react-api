79f0045c237fa531a31dfc7648792f9432158768
Emanuele Favero <emafaux89@gmail.com>
2026-07-24 19:25:12 +0200

    docs: add JSDoc example to fetchData function

--- src/lib/api.js
+++ src/lib/api.js
@@ -4,6 +4,8 @@ import axios from 'axios';
  * @param {string} url
  * @param {Object} [params]
  * @returns {Promise<any>}
+ * @example
+ * fetchData(url, { page: 1 })
  */
 export const fetchData = async (url, params = {}) => {
   const { data } = await axios.get(url, { params });
